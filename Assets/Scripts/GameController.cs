3ffee3a3843aea9090b5ff867b96411e461b0218
bdf007 <bdf007@gmail.com>
2023-04-27 12:40:33 +0200

    modify speed of enemies according to their left numbers

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,33 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
     public float shootingInterval = 3f;
     public float shootingSpeed = 2f;
     public GameObject enemyMissilePrefab;
-
+    public Player player;
     public GameObject enemyContainer;
-    public float movingInterval = 0.5f;
     public float movingDistance = 0.1f;
     public float horizontalLimit = 2.5f;
     private float movingDirection = 1.0f;
     private float movingTimer;
 
+    public float maximumMovingInterval = 0.4f;
+    public float minimumMovingInterval = 0.05f;
+
     private float shootingTimer;
+    private float movingDistanceInterval;
+    private int enemyCount;
     // Start is called before the first frame update
     void Start()
     {
+        movingDistanceInterval = maximumMovingInterval;
         shootingTimer = shootingInterval;
+        enemyCount = GetComponentsInChildren<Enemy>().Length;
     }
 
     // Update is called once per frame
     void Update()
     {
+        int currentEnemyCount = GetComponentsInChildren<Enemy>().Length;
+
         // shooting logic
         shootingTimer -= Time.deltaTime;
-        if (shootingTimer <= 0)
+        if (currentEnemyCount > 0 && shootingTimer <= 0)
         {
             shootingTimer = shootingInterval;
             Enemy[] enemies = GetComponentsInChildren<Enemy>();
@@ -45,7 +54,9 @@ public class GameController : MonoBehaviour
         movingTimer -= Time.deltaTime;
         if (movingTimer <= 0f)
         {
-            movingTimer = movingInterval;
+            float difficulty = 1f - (float)currentEnemyCount / (float)enemyCount;
+            movingDistanceInterval = maximumMovingInterval - (maximumMovingInterval - minimumMovingInterval) * difficulty;
+            movingTimer = movingDistanceInterval;
             enemyContainer.transform.position = new Vector2(enemyContainer.transform.position.x + (movingDistance * movingDirection), enemyContainer.transform.position.y);
             if (movingDirection > 0)
             {
@@ -83,6 +94,13 @@ public class GameController : MonoBehaviour
                     }
                 
             }
+        }
+
+        // restart game
+        if (currentEnemyCount == 0 || player == null)
+        {
+            // reload scene
+            SceneManager.LoadScene("Game");
         }
     }
 }
